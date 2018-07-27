139f4e2a83e076045dbfbac9d1ae467d6c1a0916
SuperSodaSea <bobby825@126.com>
2018-07-27 14:40:00 +0800

    Arrange files

---
+++ src/Python/kitteye/tokenizer.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+'''
+    Copyright (c) 2016-2018 The Cats Project
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+'''
+
+import re
+
+
+class Token:
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
+    def __str__(self):
+        return '({}, {})'.format(self.type, self.data)
+
+class Tokenizer:
+    def __init__(self):
+        self.space = re.compile('[ \t\r\n]+')
+        self.commentBegin = re.compile('/\*')
+        self.commentEnd = re.compile('\*/')
+        self.tokenTypes = [
+            ('Identifier', '[A-Z_a-z][0-9A-Z_a-z]*'),
+            ('(', '\('),
+            (')', '\)'),
+            (',', ','),
+            (':', ':'),
+            (';', ';'),
+            ('{', '{'),
+            ('}', '}'),
+            ('->', '->'),
+        ]
+        self.regexs = []
+        for tokenType in self.tokenTypes:
+            self.regexs.append(re.compile(tokenType[1]))
+    def tokenizeError(self, info):
+        raise RuntimeError(info)
+    def tokenize(self, data):
+        tokens = []
+        pos = 0
+        while pos < len(data):
+            m = self.space.match(data, pos)
+            if m:
+                pos = m.end()
+                continue
+            m = self.commentBegin.match(data, pos)
+            if m:
+                pos = m.end()
+                m = self.commentEnd.search(data, pos)
+                if not m:
+                    self.tokenizeError('Unterminated comment')
+                pos = m.end()
+                continue
+            match = False
+            for i in range(0, len(self.regexs)):
+                regex = self.regexs[i]
+                result = regex.match(data, pos)
+                if result:
+                    tokens.append(Token(self.tokenTypes[i][0], data[pos : result.end()]))
+                    pos = result.end()
+                    match = True
+                    break
+            if not match:
+                self.tokenizeError('Unknown token at ' + str(pos) + ': ' + data[pos])
+        tokens.append(Token('End', '<End>'))
+        return tokens
---
+++ src/Python/kitteye/type.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+'''
+    Copyright (c) 2016-2018 The Cats Project
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+'''
+
+class Type:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return self.name
+
+class TemplateType:
+    def __init__(self, name, arguments):
+        self.name = name
+        self.arguments = arguments
+    def __str__(self):
+        s = self.name + '('
+        first = True
+        for argument in self.arguments:
+            if not first:
+                s += ', '
+            else:
+                first = False
+            s += str(argument)
+        s += ')'
+        return s
+
+class FunctionType:
+    def __init__(self, arguments, ret):
+        self.arguments = arguments
+        self.ret = ret
+    def __str__(self):
+        s = '('
+        first = True
+        for argument in self.arguments:
+            if not first:
+                s += ', '
+            else:
+                first = False
+            s += argument[0] + ': ' + str(argument[1])
+        s += ')'
+        if self.ret is not None:
+            s += ' -> ' + str(self.ret)
+        return s
+
+class ConstantType:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    def __str__(self):
+        return 'constant ' + self.name + ' -> ' + str(self.type)
+
+class EnumType:
+    def __init__(self, name, type, enumerators):
+        self.name = name
+        self.type = type
+        self.enumerators = enumerators
+    def __str__(self):
+        s = 'enum ' + self.name + ' : ' + str(self.type) + ' {\n'
+        for enumerator in self.enumerators:
+            s += '    ' + enumerator + ',\n'
+        s += '}'
+        return s
+
+class StructType:
+    def __init__(self, name, parent, members):
+        self.name = name
+        self.parent = parent
+        self.members = members
+    def __str__(self):
+        s = 'struct ' + self.name
+        if self.parent is not None:
+            s += ' : ' + str(self.parent)
+        s += ' {\n'
+        for member in self.members:
+            s += '    ' + member[0] + ': ' + str(member[1]) + '\n'
+        s += '}'
+        return s
+
+class InterfaceType:
+    def __init__(self, name, parent, members):
+        self.name = name
+        self.parent = parent
+        self.members = members
+    def __str__(self):
+        s = 'interface ' + self.name
+        if self.parent is not None:
+            s += ' : ' + str(self.parent)
+        s += ' {\n'
+        for member in self.members:
+            s += '    ' + member[0] + ': ' + str(member[1]) + '\n'
+        s += '}'
+        return s
