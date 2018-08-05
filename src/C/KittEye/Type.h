7d355a401cab5ec598fb1a036422ba8d0edc3ed4
SuperSodaSea <bobby825@126.com>
2018-08-05 14:52:42 +0800

    Add Type.h

---
+++ src/C/KittEye/Type.h
@@ -0,0 +1,31 @@
+#ifndef KITTEYE_TYPE_H
+#define KITTEYE_TYPE_H
+
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+#define FALSE 0
+#define TRUE 1
+
+
+typedef bool Bool;
+
+typedef uint8_t Uint8;
+typedef uint16_t Uint16;
+typedef uint32_t Uint32;
+
+typedef size_t UintSize;
+
+typedef int8_t Int8;
+typedef int16_t Int16;
+typedef int32_t Int32;
+
+typedef ptrdiff_t IntSize;
+
+typedef float Float32;
+
+
+#endif
