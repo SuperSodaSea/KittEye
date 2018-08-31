a6cb1c04419945d14a384381d23a6c3ab5b8ad03
SuperSodaSea <bobby825@126.com>
2018-08-31 22:09:04 +0800

    Add Type.hpp

---
+++ src/Cpp/KittEye/Type.hpp
@@ -0,0 +1,33 @@
+#ifndef KITTEYE_TYPE_HPP
+#define KITTEYE_TYPE_HPP
+
+
+#include <cstddef>
+#include <cstdint>
+
+
+#define FALSE false
+#define TRUE true
+
+
+typedef bool Bool;
+
+typedef std::uint8_t Uint8;
+typedef std::uint16_t Uint16;
+typedef std::uint32_t Uint32;
+typedef std::uint64_t Uint64;
+
+typedef std::size_t UintSize;
+
+typedef std::int8_t Int8;
+typedef std::int16_t Int16;
+typedef std::int32_t Int32;
+typedef std::int64_t Int64;
+
+typedef std::ptrdiff_t IntSize;
+
+typedef float Float32;
+typedef double Float64;
+
+
+#endif
